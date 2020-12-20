3b8efab00dafbc0473b8fa2d6eb79ac25dbeae75
Pedro Seruca <pedroseruca+github@gmail.com>
2020-12-20 20:06:00 +0000

    Init project

---
+++ AttributedStringBuilder/AttributedStringBuilder.h
@@ -0,0 +1,18 @@
+//
+//  AttributedStringBuilder.h
+//  AttributedStringBuilder
+//
+//  Created by Pedro Seruca on 20/12/2020.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AttributedStringBuilder.
+FOUNDATION_EXPORT double AttributedStringBuilderVersionNumber;
+
+//! Project version string for AttributedStringBuilder.
+FOUNDATION_EXPORT const unsigned char AttributedStringBuilderVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AttributedStringBuilder/PublicHeader.h>
+
+
